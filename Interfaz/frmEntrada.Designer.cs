a2c02ffaed494f33c798bb68a169e83a7f7a1ac3
Michael Vargas Soto <michaelvargas47@hotmail.com>
2022-06-17 18:31:53 -0600

    Creacion de la pantalla Bienvenida

--- Interfaz/frmEntrada.Designer.cs
+++ Interfaz/frmEntrada.Designer.cs
@@ -29,20 +29,97 @@ namespace Interfaz
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEntrada));
+            this.grpVentanaEntrada = new System.Windows.Forms.GroupBox();
+            this.lblSeleccione = new System.Windows.Forms.Label();
+            this.btnModuloAdmin = new System.Windows.Forms.Button();
+            this.btnModuloUsuario = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.grpVentanaEntrada.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // grpVentanaEntrada
+            // 
+            this.grpVentanaEntrada.Controls.Add(this.pictureBox1);
+            this.grpVentanaEntrada.Controls.Add(this.btnModuloUsuario);
+            this.grpVentanaEntrada.Controls.Add(this.btnModuloAdmin);
+            this.grpVentanaEntrada.Controls.Add(this.lblSeleccione);
+            this.grpVentanaEntrada.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpVentanaEntrada.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpVentanaEntrada.Location = new System.Drawing.Point(0, 0);
+            this.grpVentanaEntrada.Name = "grpVentanaEntrada";
+            this.grpVentanaEntrada.Size = new System.Drawing.Size(378, 450);
+            this.grpVentanaEntrada.TabIndex = 0;
+            this.grpVentanaEntrada.TabStop = false;
+            // 
+            // lblSeleccione
+            // 
+            this.lblSeleccione.AutoSize = true;
+            this.lblSeleccione.Font = new System.Drawing.Font("Bauhaus 93", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeleccione.Location = new System.Drawing.Point(23, 37);
+            this.lblSeleccione.Name = "lblSeleccione";
+            this.lblSeleccione.Size = new System.Drawing.Size(313, 28);
+            this.lblSeleccione.TabIndex = 0;
+            this.lblSeleccione.Text = "Seleccione el Módulo de Uso";
+            // 
+            // btnModuloAdmin
+            // 
+            this.btnModuloAdmin.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnModuloAdmin.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnModuloAdmin.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnModuloAdmin.Location = new System.Drawing.Point(45, 99);
+            this.btnModuloAdmin.Name = "btnModuloAdmin";
+            this.btnModuloAdmin.Size = new System.Drawing.Size(251, 52);
+            this.btnModuloAdmin.TabIndex = 1;
+            this.btnModuloAdmin.Text = "Módulo Administrativo";
+            this.btnModuloAdmin.UseVisualStyleBackColor = false;
+            // 
+            // btnModuloUsuario
+            // 
+            this.btnModuloUsuario.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnModuloUsuario.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnModuloUsuario.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnModuloUsuario.Location = new System.Drawing.Point(45, 218);
+            this.btnModuloUsuario.Name = "btnModuloUsuario";
+            this.btnModuloUsuario.Size = new System.Drawing.Size(251, 52);
+            this.btnModuloUsuario.TabIndex = 2;
+            this.btnModuloUsuario.Text = "Módulo Usuario";
+            this.btnModuloUsuario.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(76, 314);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(192, 99);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
             // frmEntrada
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(378, 450);
+            this.Controls.Add(this.grpVentanaEntrada);
             this.Name = "frmEntrada";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ventana Entrada";
+            this.grpVentanaEntrada.ResumeLayout(false);
+            this.grpVentanaEntrada.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grpVentanaEntrada;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnModuloUsuario;
+        private System.Windows.Forms.Button btnModuloAdmin;
+        private System.Windows.Forms.Label lblSeleccione;
     }
 }
 
